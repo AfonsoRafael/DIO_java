c93bea0b29f461d37db95d742190997c369f11c9
Afonso Rafael <programadoafonso@gmail.com>
2026-08-31 18:32:11 -0300

    Desafio Contador DIO

---
+++ 005DesafioContado/src/Contador/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package Contador;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro.");
+    }
+}
